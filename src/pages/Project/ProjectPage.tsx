f9a78d537c177e425e5541b226bd737fe78f50ab
Cappeloow <cappe1996@hotmail.com>
2024-08-09 10:02:28 +0200

    add(projectpage): html markup for the project page

--- src/pages/Project/ProjectPage.tsx
+++ src/pages/Project/ProjectPage.tsx
@@ -3,7 +3,46 @@ import React from 'react'
 const ProjectPage = () => {
   return (
     <main>
-      Project
+      <section>
+        <aside>
+          <ul>
+            <li>
+              <a href='#'>Project 1</a>
+            </li>
+            <li>
+              <a href='#'>Project 2</a>
+            </li>
+            <li>
+              <a href='#'>Project 3</a>
+            </li>
+            <li>
+              <a href='#'>Project 4</a>
+            </li>
+            <li>
+              <a href='#'>Project 5</a>
+            </li>
+          </ul>
+        </aside>
+      </section>
+      
+      <section>
+        <div>
+          <h1>My Title</h1>
+        </div>
+        <div>
+          <div>
+            <img src="" alt="placeholder text" />
+          </div>
+          <article>Text of the project</article>
+        </div>
+        <div>Pagination
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </div>
+      </section>
     </main>
   )
 }
