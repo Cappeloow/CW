c1cfda04a8a4c874173228942b930b3faebf9f52
Cappeloow <cappe1996@hotmail.com>
2024-08-10 12:34:15 +0200

    feat(): added dynamic content to browse with pagination between categories.

---
+++ src/interfaces.ts
@@ -0,0 +1,13 @@
+export interface ICategory {
+  categoryName: string;
+  projects: IProject[];
+}
+
+export interface IProject {
+    name: string;
+    description: string;
+    img: string;
+    url: string;
+    techStack?: string[];
+    duration?: string;
+}
--- src/pages/Project/ProjectPage.tsx
+++ src/pages/Project/ProjectPage.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoMusicalNotes, IoCodeSlash } from "react-icons/io5";
 import { MdAddAPhoto, MdDesignServices } from "react-icons/md";
 import { FaQrcode } from "react-icons/fa";
+import { IoPlaySharp } from "react-icons/io5";
+import data from '../../data/projects.json';
 import project_jarekon from '../../assets/web_project_jarekon.png';
-const ProjectPage = () => {
+import { ICategory } from '../../interfaces';
+const ProjectPage = () => { 
+  const [category, setCategory] = useState<ICategory>(data[0]);
+  const [project, setProject] = useState(data[1].projects[0]);
+  console.log(data);
+
   return (
     <main className='projects'>
       <section className='left-project-section'>
         <aside>
           <ul>
-            <li className='icon-btn'>
+          <li className='icon-btn' onClick={() => setCategory(data[0])}>
               <IoCodeSlash/>
             </li>
-            <li className='icon-btn'>
+            <li className='icon-btn' onClick={() => setCategory(data[1])}>
               <IoMusicalNotes/>
             </li>
-            <li className='icon-btn'>
+            <li className='icon-btn' onClick={() => setCategory(data[2])}>
               <MdAddAPhoto/>
             </li>
-            <li className='icon-btn'>
+            <li className='icon-btn' onClick={() => setCategory(data[3])}>
               <MdDesignServices/>
             </li>
-            <li className='icon-btn'>
+            <li className='icon-btn' onClick={() => setCategory(data[4])}>
               <FaQrcode/>
             </li>
           </ul>
@@ -30,27 +37,30 @@ const ProjectPage = () => {
       
       <section className='right-project-section'>
         <div className='project-title-container'>
-          <h1>Web Development</h1>
+          <h1>{category.categoryName}</h1>
         </div>
         <div className='project-main-container'>
           <div className='project-img-container'>
-            <img src={project_jarekon} alt="" />
+            <img src={project.img} alt="" />
           </div>
           <div className='project-content-container'>
             <div>
-        
+              <h2>{project.name}</h2>
             </div>
+            <br />
             <article>
-
+              <p>{project.description}</p>
             </article>
           </div>
         </div>
         <div className='project-pagination-container'>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
+          {category.projects && category.projects.slice(0, 5).map((project, idx) => {
+            return (
+              <button key={idx} onClick={() => setProject(project)}>
+                {idx + 1} 
+              </button>
+            );
+          })}
         </div>
       </section>
     </main>
