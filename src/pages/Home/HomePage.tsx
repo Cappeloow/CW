e567df5ab8bb70e050915a9c06df569091c8357f
Cappeloow <cappe1996@hotmail.com>
2024-08-12 11:37:18 +0200

    style(homepage): responsive desktop/mobile

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -7,17 +7,19 @@ const HomePage = () => {
 
   return (
     <main className='home'>
+      <div className='main-content'>
         <section className='main-section'>
           <div className='left-main-container'>
             <ProfileHeader/>
           </div>  
-            <div className='slogan-section'>
-              <span>
-                “Let me tell you about myself.”
-              </span>
-            </div>
         </section>
         <IntroductionSection />
+      </div>
+        <div className='slogan-section'>
+            <span>
+              <i>Let me tell you about Casper</i>
+            </span>
+        </div>
     </main>
   )
 }
