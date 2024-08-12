34d043647d32f4598529be59c1dd58e205b97806
Cappeloow <cappe1996@hotmail.com>
2024-08-12 09:31:45 +0200

    feat(contact): popping up socialicons from the letter on page-enter

--- src/pages/Contact/ContactPage.tsx
+++ src/pages/Contact/ContactPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
-
+import letterfront from '../../assets/letter-front.png';
+import { FaLinkedin } from "react-icons/fa";
+import { CiMail } from "react-icons/ci";
+import letterback from '../../assets/letter-back.png';
 const ContactPage = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <main className='contact'>
+      <div className='contact-img-container'>
+        <img src={letterfront} alt="" />
+        <img src={letterback} alt="" />
+        <div className='contact-btn-container'>
+          <button className='icon-btn'><CiMail/></button>
+          <button className='icon-btn'><FaLinkedin/></button>
+          <button className='icon-btn'><CiMail/></button>
+        </div>
+      </div>
+    </main>
   )
 }
 
