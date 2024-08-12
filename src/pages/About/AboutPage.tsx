cee842bd9fd30b9f3cdcc78aabcebf0a3fae9f46
Cappeloow <cappe1996@hotmail.com>
2024-08-12 12:31:55 +0200

    style(aboutpage): responsive web and mobile

--- src/pages/About/AboutPage.tsx
+++ src/pages/About/AboutPage.tsx
@@ -8,20 +8,22 @@ import { FaLinkedin } from "react-icons/fa";
 const AboutPage = () => {
   return (
     <main className='about'>
-      <section className='about-image-section'>
-        <ProfileHeader/>
-        <div className='about-image-container'>
-          <img src={profilcasper} alt="" />
-          <div>
-            <a href="https://www.linkedin.com/in/cw1996/" target="_blank" rel="noopener noreferrer">
-              <FaLinkedin/>
-            </a>
-            <a href="https://github.com/Cappeloow" target="_blank" rel="noopener noreferrer">
-              <FaGithub/>
-            </a>
+      <div className='container'>
+        <section className='about-image-section'>
+          <ProfileHeader/>
+          <div className='about-image-container'>
+            <img src={profilcasper} alt="" />
+            <div>
+              <a href="https://www.linkedin.com/in/cw1996/" target="_blank" rel="noopener noreferrer">
+                <FaLinkedin/>
+              </a>
+              <a href="https://github.com/Cappeloow" target="_blank" rel="noopener noreferrer">
+                <FaGithub/>
+              </a>
+            </div>        
           </div>
-        </div>
-      </section>
+        </section>
+
     
       <section className='about-text-section'>
         <h2>About Me</h2>
@@ -33,6 +35,7 @@ const AboutPage = () => {
           My passion for creating bold, engaging, and visually stunning visuals started when I was a teenager. I used to spend hours sketching and painting, trying to capture the essence of different cultures and their stories. Over time, I developed a unique style and started selling my work at local galleries and art fairs.
         </p>
       </section>
+    </div>
     </main>
   )
 }
