50779d9a80971f4fda2dd71c977797bfc158d87b
Cappeloow <cappe1996@hotmail.com>
2024-08-15 11:31:30 +0200

    style(footer): added content to the footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { FaLinkedin, FaGithub, FaTwitter, FaLocationArrow, FaMailchimp, FaAmilia, FaHome } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { LayoutRouteProps } from 'react-router-dom';
 import cwIcon from '../assets/cw_icon.png';
+import { CiMail, CiUser } from 'react-icons/ci';
 const Layout = ({ children }:LayoutRouteProps) => {
  return (
  <>
@@ -22,7 +25,34 @@ const Layout = ({ children }:LayoutRouteProps) => {
  {children}
  </>
  <footer>
-   <p>Footer</p>
+   <div className='main-footer-container'>
+   <section className='social-media-section'>
+    <a href="https://www.linkedin.com/in/cw1996/" target="_blank" rel="noopener noreferrer">
+     <FaLinkedin/>
+    </a>
+    <a href="https://github.com/Cappeloow" target="_blank" rel="noopener noreferrer">
+     <FaGithub/>
+    </a>
+    <a href="https://twitter.com/Cappeloow" target="_blank" rel="noopener noreferrer">
+     <FaTwitter/>
+    </a>
+   </section>
+   <section className='general-info-section'>
+    <div>
+      <p><CiUser/> Casper Wernersson</p>
+      <p><FaHome/> Karlshamn</p>
+      <p><a href="mailto:casper.wernersson@medieinstitutet.se"><CiMail/> Casper.wernersson@medieinstitutet.se</a></p>
+    </div>
+   </section>
+   <section className='quick-links-section'>
+    <li><a href="/about"><FaLocationArrow/> About</a></li>
+    <li><a href="/projects"><FaLocationArrow/> Projects</a></li>
+    <li><a href="/contact"><FaLocationArrow/> Contact</a></li>
+    </section>
+    </div>
+    <div className='copyright-container'>
+      <p>Copyright © 2024 Casper Wernersson</p>
+    </div>
  </footer>
  </>
  );
